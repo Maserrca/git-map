ee29515073450c42b2af8c8e001269ee02f162af
maserrca <marina.serranoc@gmail.com>
2021-06-11 13:40:15 +0200

    paso 3

--- api.py
+++ api.py
@@ -11,12 +11,10 @@ def landing_page():
 @app.route("/dashboard")
 def dashboard():
     return render_template('dashboard.html')
+
 @app.route("/map")
 def map():
     return render_template('map.html')
-
-
-
 
 
 
